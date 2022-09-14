68ba42bc0fd5df4fad3d19fcdcfec22019c75d82
codekriss <70336161+codekriss@users.noreply.github.com>
2022-09-14 09:56:56 +0200

    Update ParsesProperties.php

--- src/Bootstrap/Concerns/ParsesProperties.php
+++ src/Bootstrap/Concerns/ParsesProperties.php
@@ -8,15 +8,33 @@ trait ParsesProperties
 {
     protected function parseProperty(string $property, $data)
     {
-        $value = data_get($data, $property, null);
+        $value = null;
+
+        try {
+            $value = $value ?? $data->{$property};
+        } catch (Throwable $e) {
+            //
+        }
+
+        try {
+            $value = $value ?? $data[$property];
+        } catch (Throwable $e) {
+            //
+        }
+
+        $value = $value ?? data_get($data, $property, $value);
 
         if (!$value && is_object($data)) {
-            if (method_exists($data, $property)) {
-                $value = $data->{$property}();
-            }
+            try {
+                if (method_exists($data, $property)) {
+                    $value = $data->{$property}();
+                }
 
-            if (!$value && method_exists($data, 'get' . ucfirst($property) . 'Attribute')) {
-                $value = $data->{'get' . ucfirst($property) . 'Attribute'}();
+                if (!$value && method_exists($data, 'get' . ucfirst($property) . 'Attribute')) {
+                    $value = $data->{'get' . ucfirst($property) . 'Attribute'}();
+                }
+            } catch (Throwable $e) {
+                //
             }
         }
 
